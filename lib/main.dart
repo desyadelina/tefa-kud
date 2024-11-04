0516b16fce7cd9e105a799a8ea9199a1deb8e979
Azizah Nur Octaviani <154779986+viazah@users.noreply.github.com>
2024-11-04 19:22:21 +0800

    add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _isMoved = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _isMoved = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: Duration(seconds: 2),
+        alignment: _isMoved ? Alignment(-1.0, -1.0) : Alignment.center,
+        child: AnimatedScale(
+          scale: _isMoved ? 0.5 : 1.0,
+          duration: Duration(seconds: 2),
+          curve: Curves.easeInOut,
+          child: Image.asset('assets/logo.png', width: 100, height: 100),
         ),
       ),
     );
