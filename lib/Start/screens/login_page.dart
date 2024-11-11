6a7101cecde319308c06d7b386914e30c67ac023
O12097 <desyaderina@gmail.com>
2024-11-11 20:52:40 +0800

    style: change snackbar language from english to indonesian

--- lib/Start/screens/login_page.dart
+++ lib/Start/screens/login_page.dart
@@ -64,7 +64,7 @@ class _LoginFormState extends State<LoginScreen> {
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
-            content: Text('Login failed. Please check your credentials.')),
+            content: Text('Gagal masuk. Silahkan masukkan data anda dengan benar.')),
       );
     }
   }
