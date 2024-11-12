88db197d3fbc53e134c852fae1af45b8dae4773e
agungmardika <agungdmardika@gmail.com>
2024-11-13 06:47:26 +0800

    interface: transfer done. isi saldo 50%

--- lib/Start/screens/isi_saldo/receipt_isi_saldo.dart
+++ lib/Start/screens/isi_saldo/receipt_isi_saldo.dart
@@ -19,56 +19,57 @@ class _ReceiptIsiSaldoState extends State<ReceiptIsiSaldo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFF2F2F2),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 67, 150, 79),
-          title: const Text(
-            'Bukti Isi Saldo',
-            style: TextStyle(color: Colors.white),
-          ),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+      backgroundColor: Color(0xFFF2F2F2),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 67, 150, 79),
+        title: const Text(
+          'Bukti Isi Saldo',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/Transfer.png',
+                    color: Colors.green,
+                    width: 30,
+                    height: 30,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Nominal Isi Saldo',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Text(
+                "909090999",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
           ),
         ),
-        body: Container(
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/Transfer.png',
-                      color: Colors.green,
-                      width: 30,
-                      height: 30,
-                    ),
-                    const SizedBox(width: 8),
-                    const Text(
-                      'Nominal Isi Saldo',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  "909090999",
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ));
+      ),
+    );
   }
 }
