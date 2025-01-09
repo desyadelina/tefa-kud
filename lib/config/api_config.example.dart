0557c9ad211d5b4be061d64b30d72f416287c697
Rushel <ruselaq@gmail.com>
2025-01-09 20:38:25 +0800

    feat: update API base URL use my own server

--- lib/config/api_config.example.dart
+++ lib/config/api_config.example.dart
@@ -1,4 +1,4 @@
 // make new file named api_config.dart in the same directory and copy the content of this file to api_config.dart
 class ApiConfig {
-  static const String baseUrl = 'http://192.168.18.10:8000/api';
+  static const String baseUrl = 'https://kud.rushel.my.id/api';
 }
